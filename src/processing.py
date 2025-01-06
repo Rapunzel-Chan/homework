dde5d7ff932d533728d7035dfbda55e283a3072a
Rapunzel <osina_kv@mail.ru>
2025-01-06 16:13:37 +0300

    add new filter func by date

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,14 @@
 def filter_by_state(bank_operations: list, state='EXECUTED') -> list:
     """Функция возвращает список словарей, отфильтрованных по заданному параметру"""
-    filtered_id = []
+    filtered_id_state = []
     for operation in bank_operations:
         if operation['state'] == state:
-            filtered_id.append(operation)
-    return filtered_id
+            filtered_id_state.append(operation)
+    return filtered_id_state
+
+def sort_by_date(bank_operations: list, reverse=True) -> list:
+    filtered_id_date = sorted(bank_operations, key=lambda operation: operation["date"], reverse=reverse)
+    return filtered_id_date
 
 
 if __name__ == '__main__':
@@ -19,3 +23,11 @@ if __name__ == '__main__':
                                   state='CANCELED')
     print(operation_state_ex)
     print(operation_state_can)
+    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], reverse=False))
