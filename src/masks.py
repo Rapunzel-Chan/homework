3459a452d7cc2b6cf09a856c5e7b144d73352268
Rapunzel <osina_kv@mail.ru>
2025-01-21 21:00:22 +0300

    create widget tests and prepare conftest, fix masks

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,13 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует номера банковской карты"""
     card_numbers =''.join(num if num.isdigit() else '' for num in card_number)
-    if len(card_numbers) == 0:
-        raise ValueError("Вы не ввели номер карты")
-    if len(card_numbers) != 16:
-        raise ValueError("Неверный номер карты")
-    else:
+    if len(card_numbers) == 16:
         return f"{card_number[:-17]}{card_number[-17:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
+    else:
+        if len(card_numbers) == 0:
+            raise ValueError("Вы не ввели номер карты")
+        else:
+            raise ValueError("Неверный номер карты")
 
 
 def get_mask_account(account_number: str) -> str:
@@ -20,6 +21,5 @@ def get_mask_account(account_number: str) -> str:
 
 if __name__ == '__main__':
     print(get_mask_card_number('Maestro 1596837868705199'))
-    print(get_mask_card_number(''))
     print(get_mask_card_number('Visa Classic 6831982476737658'))
     print(get_mask_account('Счет 73654108430135874305'))
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,7 +4,7 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 def test_get_mask_card_number():
-    assert get_mask_card_number('Visa Classic 6831982476737658')
+    assert get_mask_card_number('Maestro 6831982476737658') == 'Maestro 6831 98** **** 7658'
 
 def test_get_mask_card_number_wrong_card_number():
     with pytest.raises(ValueError):
@@ -17,7 +17,7 @@ def test_get_mask_card_number_empty_card_number():
 
 
 def test_get_mask_account():
-    assert get_mask_account('Счет 73654108430135874305')
+    assert get_mask_account('Счет 73654108430135874305') == 'Счет **4305'
 
 
 def test_get_mask_account_wrong_account_number():
