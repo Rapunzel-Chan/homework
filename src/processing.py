9deb133b9bdaf19dd4a412126445d184acd72d19
Rapunzel <osina_kv@mail.ru>
2025-01-21 23:05:04 +0300

    creating processing tests

--- src/processing.py
+++ src/processing.py
@@ -1,24 +1,33 @@
-def filter_by_state(bank_operations: list, state: str = 'EXECUTED') -> list:
+def filter_by_state(bank_operations: list, state: str) -> list:
     """Функция возвращает список словарей, отсортированных по заданному параметру"""
     filtered_id_state = []
     for operation in bank_operations:
         if operation['state'] == state:
             filtered_id_state.append(operation)
-    return filtered_id_state
+    if filtered_id_state != []:
+        return filtered_id_state
+    else:
+        raise KeyError('Словарей для такого статуса нет')
 
 
 def sort_by_date(bank_operations: list, reverse: bool = True) -> list:
     """Функция возвращает список словарей, отсортированных по дате"""
     filtered_id_date = sorted(bank_operations, key=lambda operation: operation["date"], reverse=reverse)
+    for operation in bank_operations:
+        date_length = operation['date']
+        if len(date_length) == 26:
+            continue
+        else:
+            raise ValueError("Неверный формат даты")
     return filtered_id_date
-
 
 if __name__ == '__main__':
     operation_state_ex = filter_by_state(
         [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+    state='EXECUTED')
     operation_state_can = filter_by_state(
         [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
