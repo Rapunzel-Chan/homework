3130ba4447b74ed43609c6542cf37657b1e0fe3e
Rapunzel <osina_kv@mail.ru>
2025-01-21 21:19:59 +0300

    fix widget test with data

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -30,13 +30,12 @@ def test_get_mask_account_card_empty_account_number():
     with pytest.raises(ValueError):
         get_mask_account_card('')
 
-
-
-
-
-
-
 def test_get_data():
     assert get_data('2024-03-11T02:26:18.671407') == '11.03.2024'
+
+
+def test_get_data_wrong():
+    with pytest.raises(ValueError):
+        get_data('')
 
 
